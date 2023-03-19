56214a6f04acb2e0e43f72c15e9397f79d4130c5
saira512dev <512saira@gmail.com>
2023-03-19 10:00:00 -0600

    Removed unwanted routes

--- routes/sales.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getSales } from "../controllers/sales.js"
-import ensureAuth from "../middleware/auth.js";
-
-const router = express.Router();
-
-router.get("/sales", ensureAuth, getSales)
-
-export default router;
