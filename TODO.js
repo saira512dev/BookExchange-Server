29d5382907b339a01ff33d50cc1d0a8f3e6682bb
saira512dev <512saira@gmail.com>
2023-03-22 10:00:00 -0600

    Added a todo list for the backend

---
+++ TODO.js
@@ -0,0 +1,12 @@
+//TODO
+//Set up new db
+//Add books model
+//Check and modify user model
+//Add books route
+//Add api to add books
+//Add search api in books controller
+  //Returns the searched book with available or not status
+
+//------------Improvements----------------------
+//Google auth could be added
+//Learn and implement testing
